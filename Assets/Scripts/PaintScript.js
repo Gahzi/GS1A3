22df7653b6ca58eea84ab1cc9f5237d2e22c35b3
Shervin Ghazazani <shervinghazazani@gmail.com>
2012-11-20 03:48:43 -0500

    Added paintcan, player painting and cleaned up the project a bit

--- Assets/Scripts/PaintScript.js
+++ Assets/Scripts/PaintScript.js
@@ -2,41 +2,32 @@
 // on whatever the user clicks. Make sure the mesh you want to paint 
 // on has a mesh collider attached.
 function Update () {
+	
     // Only when we press the mouse
     if (!Input.GetMouseButton (0))
         return;
-
+	
     // Only if we hit something, do we continue
     var hit : RaycastHit;
-    if (!Physics.Raycast (camera.ScreenPointToRay(Input.mousePosition), hit))
+    
+    if (!Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition),hit,7.0f)) {
+    	Debug.Log("Raycast Missed!");
         return;
-
+    }
     // Just in case, also make sure the collider also has a renderer
     // material and texture. Also we should ignore primitive colliders.
     var renderer : Renderer = hit.collider.renderer;
-       var boxCollider = hit.collider as BoxCollider;
+    var boxCollider = hit.collider as MeshCollider;
+	
     if (renderer == null || renderer.sharedMaterial == null ||
-        renderer.sharedMaterial.mainTexture == null || boxCollider == null)
-        if(renderer == null) {
-        	Debug.Log("renderer is null");
-        }
-        else if(renderer.sharedMaterial == null) {
-        	Debug.Log("renderer.SharedMaterial is null");
-        }
-        else if(renderer.sharedMaterial.mainTexture == null) {
-        	Debug.Log("renderer.sharedMaterial.mainTexture is null");
-        }
-        else if(boxCollider == null) {
-        	Debug.Log("boxCollider is null");
-        }
+        renderer.sharedMaterial.mainTexture == null || boxCollider == null) {
         return;
- 
+ 	}
     // Now draw a pixel where we hit the object
     var tex : Texture2D = renderer.material.mainTexture;
     var pixelUV = hit.textureCoord;
     pixelUV.x *= tex.width;
     pixelUV.y *= tex.height;
-    
     tex.SetPixel(pixelUV.x, pixelUV.y, Color.red);
  
     tex.Apply();
--- Assets/Scripts/PickUpObjectScript.js
+++ Assets/Scripts/PickUpObjectScript.js
@@ -27,7 +27,7 @@ function Update(){
 	                
 	                pickObj = hit.transform; // now there's an object picked
 	                pickObj.gameObject.renderer.enabled = false;
-	                pickObj.active = false;
+	                pickObj.gameObject.active = false;
 	                //Debug.Log("renderer is false");
 	                // remember its distance from the camera
 	                dist = Vector3.Distance(pickObj.position, Camera.main.transform.position);
